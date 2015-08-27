c321ff84bdf9d380ef3803d28d07f3997295556c
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2015-08-27 14:49:52 +0800

    Initial commit
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

---
+++ debug.go
@@ -0,0 +1,40 @@
+package debug
+
+import (
+	"log"
+	"runtime"
+)
+
+// Log messages
+func Log(msg ...interface{}) {
+	file, line, fn := trace()
+	log.Printf("log: %s(%d): %s: %v\n", file, line, fn, msg)
+}
+
+// Prints out warning message along with trace information
+func Warn(msg interface{}) {
+	file, line, fn := trace()
+	log.Printf("warning: %s(%d): %s: %v\n", file, line, fn, msg)
+}
+
+// Prints out error message along with trace information.
+func Error(err error) {
+	file, line, fn := trace()
+	log.Printf("error: %s(%d): %s: %v\n", file, line, fn, err)
+}
+
+// Prints out error message along with trace information and terminates the program.
+func Fatal(err error) {
+	file, line, fn := trace()
+	log.Fatalf("error: %s(%d): %s: %v\n", file, line, fn, err)
+}
+
+// Trace current function name along with its file and line number
+func trace() (file string, line int, funcName string) {
+	pc := make([]uintptr, 10)
+	runtime.Callers(3, pc)
+	fn := runtime.FuncForPC(pc[0])
+	file, line = fn.FileLine(pc[0])
+	funcName = fn.Name()
+	return
+}
